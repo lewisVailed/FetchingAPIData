536d3a917fbe0b1bea4afddcf87f5e06fd0bdd09
Ayberk Bilgiç <lewidvailed@outlook.com>
2024-02-26 13:35:44 +0300

    delegate, datasource functions update

--- FetchingAPIData/FetchingAPIData/Controllers/HomeController.swift
+++ FetchingAPIData/FetchingAPIData/Controllers/HomeController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    // MARK: - Variables
+    private let coins: [Coin] = Coin.getMockArray()
+    
+    
+    // MARK: - UI Components
+    private let tableView: UITableView = {
+        let tableview = UITableView()
+        tableview.backgroundColor = .systemBackground
+        tableview.register(CoinCell.self, forCellReuseIdentifier: CoinCell.identifier)
+        return tableview
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
+        self.setupUI()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
     }
 
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor)
+        ])
+    }
 
 }
 
+// MARK: - Table View Functions
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.coins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CoinCell.identifier, for: indexPath) as? CoinCell else {
+            fatalError("Unable to dequeue CoinCell in HomeController")
+        }
+        let coin = self.coins[indexPath.row]
+        cell.configure(with: coin)
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
